eb97ad34002359491c4612d51697a41c0b103ce9
Sandhanaraj Navin <navinsandhanraj@gmail.com>
2025-06-20 16:03:05 +0530

    checking

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,9 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
+console.log('🔥 ENV VARS:', import.meta.env);
+console.log('✅ Firebase Key:', import.meta.env.VITE_FIREBASE_API_KEY);
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
